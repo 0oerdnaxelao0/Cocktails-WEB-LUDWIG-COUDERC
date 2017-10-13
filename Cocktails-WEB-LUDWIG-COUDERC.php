f79de5a1c1d8312ba2ed8694fcd76bb77e5bb754
0oerdnaxelao0 <alexandre.ludwig3@etu.univ-lorraine.fr>
2017-10-13 16:42:04 +0200

    Base HTML

---
+++ Cocktails-WEB-LUDWIG-COUDERC.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<?php
+
+?>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Cocktails</title>
+    </head>
+    <body>
+    <p>
+
+    </p>
+    </body>
+</html>
